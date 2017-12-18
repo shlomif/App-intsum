72800f2040d0220f933298f769ca166a1d286cf9
Shlomi Fish <shlomif@shlomifish.org>
2017-12-18 22:56:53 +0200

    Got the tests to pass.

---
+++ App-intrunningsum/lib/App/intrunningsum.pm
@@ -0,0 +1,72 @@
+package App::intrunningsum;
+
+use strict;
+use warnings;
+
+use Math::GMP ();
+
+sub new
+{
+    my $class = shift;
+
+    my $self = bless {}, $class;
+
+    $self->_init(@_);
+
+    return $self;
+}
+
+sub _init
+{
+    my ($self, $args) = @_;
+
+    return;
+}
+
+sub run
+{
+    my $s = Math::GMP->new('0');
+
+    while (my $l = <ARGV>)
+    {
+        chomp$l;
+        $s += Math::GMP->new($l);
+    }
+
+    print "$s\n";
+
+    return;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::intrunningsum - running sum of decimal big integers from stdin or files
+
+=head1 SYNOPSIS
+
+    $ (echo 2 ; echo 3) | intsum
+    2
+    5
+
+=head1 DESCRIPTION
+
+Performs an arithmetic sum of decimal integers in the files given as command
+line arguments and STDIN.
+
+=head1 VERSION
+
+=head1 METHODS
+
+=head2 new
+
+Constructor - for internal use.
+
+=head2 run
+
+Run the app - for internal use.
+=cut
+
